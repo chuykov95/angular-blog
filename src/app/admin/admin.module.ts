7fd4257afbce1aeae7f324824a892bc841bb144e
Роман <chuykov95@mail.ru>
2021-10-26 23:32:27 +0300

    open page, delete and open function

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,12 +8,12 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import {AuthService} from './shared/services/auth.service'
 import {SharedModule} from '../shared/shared.module'
 import {AuthGuard} from './shared/services/auth.guard'
+import {SearchPipe} from './shared/search.pipe'
 
 @NgModule({
-  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
+  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent, SearchPipe],
   imports: [
     CommonModule,
     FormsModule,
